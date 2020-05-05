5753f7e855fdc3c4423d265326b9785b04dd2ad5
carden <carden.ruby@gmail.com>
2020-05-05 18:29:55 +0300

    /about/author

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/about', to: 'info_pages#about'
+  get '/about/author', to: 'info_pages#author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
