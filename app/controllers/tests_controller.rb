da6da1ba2ce25985c854a5c2cdd85b80e39e0321
carden <carden.ruby@gmail.com>
2020-05-31 10:58:19 +0300

    correction

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -40,8 +40,8 @@ class TestsController < ApplicationController
   end
 
   def start
-    @current_user.tests.push(@test)
-    redirect_to @current_user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
