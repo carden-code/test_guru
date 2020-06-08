4d2829506ec961f067497fc12abaa7f29822e501
carden <carden.ruby@gmail.com>
2020-06-09 01:45:39 +0300

    bootstrap'

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,6 +28,6 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def message_after_login
-    flash[:notice] = I18n.t('success', user: current_user.first_name) # "Hello, #{current_user.first_name}!"
+    flash[:success] = I18n.t('success', user: current_user.first_name) # "Hello, #{current_user.first_name}!"
   end
 end
