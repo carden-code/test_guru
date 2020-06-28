871866fa6f1e13f0fe6b49118449170aa5727ce5
carden <carden.ruby@gmail.com>
2020-06-28 14:27:24 +0300

    feedback

--- app/helpers/tests_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module TestsHelper
-  TEST_LEVELS = { 0 || 1 => I18n.t('easy'), 2 || 3 || 4 => I18n.t('medium') }.freeze
-  def test_level(test)
-    TEST_LEVELS[test.level] || I18n.t('hard')
-  end
-end
--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: %('TestGuru' <mail@testguru.com>)
-  layout 'mailer'
-end
