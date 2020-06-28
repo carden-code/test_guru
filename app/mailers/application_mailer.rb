05447c7024a401abf77a8d4849da91fbb85a17c2
carden <carden.ruby@gmail.com>
2020-06-28 21:34:48 +0300

    mailer edit

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: %('TestGuru' <yourtests.herokuapp.com>)
+  default from: 'yourtestsapp@gmail.com'
   layout 'mailer'
 end
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,7 +1,7 @@
 class FeedbackMailer < ApplicationMailer
   def send_feedback
     message = params.text.concat("\nFrom #{params.email}")
-
+    
     mail(to: 'yourtestsapp@gmail.com', subject: 'Feedback from TestGuru') do |format|
       format.text { render plain: message }
     end
