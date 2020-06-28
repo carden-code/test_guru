fcdc5ff6fc85d34269310325eb04a6c66151697b
carden <carden.ruby@gmail.com>
2020-06-28 20:30:58 +0300

    modif config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'yourtests.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'yourtests.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
