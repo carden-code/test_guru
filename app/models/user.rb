93c3f3e8046fc41a821f345c9f9b939ab63b3451
carden <carden.ruby@gmail.com>
2020-06-30 23:04:29 +0300

    modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages
+  has_many :tests, through: :test_passages, dependent: :destroy
   has_many :gists
 
   # Атрибут name не могут иметь пустое значение
